f7fd45c0522d616f0e8357fda636cebcab013c06
Nrsimha <petr.vacha@gmail.com>
2018-10-26 22:34:47 +0200

    Added Markdown support, create page from markdown and blog index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,63 @@
 import React from "react";
+import { Link, graphql } from "gatsby";
+import { css } from "react-emotion";
+import { rhythm } from "../utils/typography";
 import Layout from "../components/layout";
 
-export default () => (
-  <Layout>
-    <p>What a world.</p>
-    <p>
-      From Richard Hamming’s classic and must-read talk, “
-      <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-        You and Your Research
-      </a>
-      ”.
-    </p>
-    <blockquote>
-      <p>
-        There is indeed an element of luck, and no, there isn’t. The prepared
-        mind sooner or later finds something important and does it. So yes, it
-        is luck.{" "}
-        <em>
-          The particular thing you do is luck, but that you do something is not.
-        </em>
-      </p>
-    </blockquote>
-    <p>Posted April 09, 2011</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </Layout>
-);
+export default ({ data }) => {
+  return (
+    <Layout>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3
+            className={css`
+              margin-bottom: ${rhythm(1 / 4)};
+            `}
+          >
+            <Link
+              to={node.fields.slug}
+              className={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              {node.frontmatter.title}{" "}
+            </Link>
+            <span
+              className={css`
+                color: #bbb;
+              `}
+            >
+              — {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+      <h2>Random Image</h2>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
